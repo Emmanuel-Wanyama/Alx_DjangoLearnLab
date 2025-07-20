30096c1cd5001019c2912f6e71ffb295d8f35c7f
Emmanuel <ejawanyama@gmail.com>
2025-07-20 22:36:51 +0300

    Views.py update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -29,7 +29,7 @@ class LibraryDetailView(DetailView):
     It uses Django's DetailView to retrieve a single Library object.
     """
     model = Library
-    template_name = 'relationship_app/library_detail.html' # Path to your template
+    template_name = 'relationship_app/library_detail.html' # This view should render a simple text list of book titles and their authors.
 
     def get_context_data(self, **kwargs):
         """
