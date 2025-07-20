1d12d11d324b65d98013f5f88bad3bae560c1488
Emmanuel <ejawanyama@gmail.com>
2025-07-20 22:50:44 +0300

    try again

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,35 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import DetailView # Import DetailView
+# relationship_app/views.py
+
+from django.shortcuts import render # Keep render for other potential uses, though not for book_list now
+from django.http import HttpResponse # Ensure HttpResponse is imported
+from django.views.generic import DetailView
 from .models import Book, Library # Import Library model
 
 def book_list(request):
     """
     A function-based view that retrieves all books from the database
-    and renders them as a simple text list of titles and authors.
+    and renders them as a simple plain text list of titles and authors.
     """
     books = Book.objects.all().select_related('author') # Select related author to avoid N+1 queries
     
-    # Build a string with book titles and authors
-    book_titles_authors = "<h1>All Books in the Database</h1>"
+    # Build a simple plain text string with book titles and authors
+    book_titles_authors = "All Books in the Database:\n\n"
     if books:
-        book_titles_authors += "<ul>"
         for book in books:
-            book_titles_authors += f"<li>{book.title} by {book.author.name}</li>"
-        book_titles_authors += "</ul>"
+            book_titles_authors += f"- {book.title} by {book.author.name}\n"
     else:
-        book_titles_authors += "<p>No books found in the database.</p>"
+        book_titles_authors += "No books found in the database.\n"
         
-    return HttpResponse(book_titles_authors)
+    return HttpResponse(book_titles_authors, content_type="text/plain")
 
 class LibraryDetailView(DetailView):
     """
     A class-based view that displays details for a specific library,
-    listing all books available in that library.
+    listing all books available in that library, using an HTML template.
     It uses Django's DetailView to retrieve a single Library object.
     """
     model = Library
-    template_name = 'relationship_app/library_detail.html' # This view should render a simple text list of book titles and their authors.
+    template_name = 'relationship_app/library_detail.html' # Path to your template
 
     def get_context_data(self, **kwargs):
         """
@@ -38,7 +38,9 @@ class LibraryDetailView(DetailView):
         """
         context = super().get_context_data(**kwargs)
         # The 'object' in context is the Library instance retrieved by DetailView
-        context['books_in_library'] = self.object.books.all().select_related('author')
+        # The template can access books directly via {{ library.books.all }}
+        # but explicitly adding it to context can sometimes be clearer.
+        # For ManyToMany, accessing directly in template is usually fine.
+        # context['books_in_library'] = self.object.books.all().select_related('author')
         return context
 
-# Create your views here.
