c19b98c733e682c71fd3ae71369211b75d8fae74
Emmanuel <ejawanyama@gmail.com>
2025-07-22 22:36:30 +0300

    5

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import user_passes_test, permission_required
 from django import forms
+from .forms import ExampleForm
 
 
 # Simple form for adding/editing books
