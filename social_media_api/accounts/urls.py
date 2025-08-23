e92c9c1f75b15e1fe8ef0e19dedd10b4898bd563
Emmanuel <ejawanyama@gmail.com>
2025-08-23 22:33:32 +0300

    m

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
-    path('follow/<int:pk>/', FollowUserView.as_view(), name='follow-user'),
-    path('unfollow/<int:pk>/', UnfollowUserView.as_view(), name='unfollow-user'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
+
+'''unfollow/<int:user_id>/", "follow/<int:user_id>'''
